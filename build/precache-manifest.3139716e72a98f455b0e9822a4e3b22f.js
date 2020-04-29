f14cbbafc20a07a637eae146175219738228abdf
Jose Carlos da Silva Filho <53589773+jose-onfk@users.noreply.github.com>
2020-04-29 11:41:58 -0400

    Builded application

---
+++ build/precache-manifest.3139716e72a98f455b0e9822a4e3b22f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd36bf1c5867cb8992e80568695a015",
+    "url": "/index.html"
+  },
+  {
+    "revision": "452139cc4d81129de09d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e17a6f1bff1cf11f10e",
+    "url": "/static/js/2.735d7c95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.735d7c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452139cc4d81129de09d",
+    "url": "/static/js/main.64edb9be.chunk.js"
+  },
+  {
+    "revision": "a803ed6fb901f93269a1",
+    "url": "/static/js/runtime-main.9e75ba94.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
